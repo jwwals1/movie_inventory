86e17cadf4c0d6bbefe15123964ed04a479eba85
jwwals1 <joe_walsh59@yahoo.com>
2024-06-01 12:57:34 -0500

    added controllers folder

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
 var app = express();
+
+
+
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://joewalsh59:Ottawa1624@cluster0.nlhkyy3.mongodb.net/music_inventory?retryWrites=true&w=majority&appName=Cluster0";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
